d7063596acf2a4a98834860093d950a65815fd39
John Zarek <jpzgoku@hotmail.com>
2017-02-11 10:31:37 -0600

    30th commit

---
+++ python/10-19/problem10.py
@@ -0,0 +1,12 @@
+# sieve of eratosthenes
+def primes(n):
+    ps, sieve = [], [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+           ps.append(p)
+           for i in range(p * p, n + 1, p):
+               sieve[i] = False
+    return ps
+
+
+print(sum(primes(2000000)))
