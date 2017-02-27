88cfd14fadc64598f09852696319ac7500c84374
John Zarek <jpzgoku@hotmail.com>
2017-02-26 20:09:27 -0600

    34th commit

--- python/1-9/problem1.py
+++ python/1-9/problem1.py
@@ -1,5 +1,6 @@
 # Returns an array of all multiples of 3 and 5 up to but not including x.
-multiples_of_three_and_five = lambda n: [x for x in range(n) if x % 3 == 0 or x % 5 == 0]
+def multiples_of_three_and_five(n):
+	return [x for x in range(n) if x % 3 == 0 or x % 5 == 0]
 
 
 print(sum(multiples_of_three_and_five(1000)))
